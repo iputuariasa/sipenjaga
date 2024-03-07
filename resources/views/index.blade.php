4e0bc6d3d0273499a1731c744b5922cb51a52e11
iputuariasa <iputuariasa75@gmail.com>
2024-03-07 17:08:08 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Platform Pengajuan Digital Nusamba Tegallalang</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
+        rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link href="css/templatemo-pod-talk.css" rel="stylesheet">
+
+    <!--
+
+TemplateMo 584 Pod Talk
+
+https://templatemo.com/tm-584-pod-talk
+
+-->
+</head>
+
+<body>
+
+    <main>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand me-lg-5 me-0" href="index.html">
+                    <img src="img/logoputih.png" class="logo-image img-fluid" alt="templatemo pod talk">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.html">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://bprnusamba-tegallalang.com/">Website Kami</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">Sosial Media</a>
+
+                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                                <li><a class="dropdown-item" href="https://www.instagram.com/nusambategallalang/">Instagram</a></li>
+                                <li><a class="dropdown-item" href="https://www.facebook.com/bprnusambategallalang1990/">Facebook</a></li>
+                                <li><a class="dropdown-item" href="https://api.whatsapp.com/send/?phone=6281239726026&text=Tertarik+dengan+produk+PT.+BPR+Nusamaba+Tegallalang%3F&type=phone_number&app_absent=0">Whatsapp</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Telp</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <section class="hero-section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="text-center mb-5 pb-2">
+                            <h1 class="text-white">Bank Nusamba Tegallalang</h1>
+
+                            <p class="text-white">Platform Digital Pengajuan Kredit, Tabungan, Deposito</p>
+
+                            <a href="#section_2" class="btn custom-btn smoothscroll mt-3">Buku Panduan</a>
+                        </div>
+
+                        <div class="owl-carousel owl-theme">
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="img/kredit.png"
+                                    class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info text-center">
+                                    <h4 class="mb-2">
+                                        Kredit
+                                        <!-- <img src="images/verified.png" class="owl-carousel-verified-image img-fluid" alt=""> -->
+                                    </h4>
+
+                                    <a href="" class="badge w-100 ">Ajukan</a>
+                                </div>
+                            </div>
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="img/tabungan.png"
+                                    class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info text-center ">
+                                    <h4 class="mb-2">
+                                        Tabungan
+                                        <!-- <img src="images/verified.png" class="owl-carousel-verified-image img-fluid" accordion =""> -->
+                                    </h4>
+
+                                    <a href="" class="badge w-100 ">Ajukan</a>
+                                </div>
+                            </div>
+
+                            <div class="owl-carousel-info-wrap item">
+                                <img src="img/deposito.png"
+                                    class="owl-carousel-image img-fluid" alt="">
+
+                                <div class="owl-carousel-info text-center ">
+                                    <h4 class="mb-2">Deposito</h4>
+
+                                    <a href="" class="badge w-100 ">Ajukan</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 col-12 mb-4 mb-md-0 mb-lg-0 text-center ">
+                    <h6 class="site-footer-title mb-3">Contact</h6>
+
+                    <p class="mb-2"><strong class="d-inline me-2">Phone:</strong> 0361980805</p>
+
+                    <p>
+                        <strong class="d-inline me-2">Email:</strong>
+                        <a href="#">nsbtgl@gmail.com</a>
+                    </p>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-12 d-flex flex-column justify-content-center align-items-center ">
+                    <h6 class="site-footer-title mb-3">Alamat</h6>
+
+                    <div class="site-footer-thumb mb-4 pb-2">
+                        <div class="d-flex flex-wrap">
+                            <iframe style="height: 160px;" class="w-100 " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1973.173245132205!2d115.27501771513943!3d-8.465646899999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2227e4d8530c9%3A0xf17f0b7d4a9e7fc!2sBPR%20Nusamba%20Tegallalang%20KPO!5e0!3m2!1sid!2sid!4v1709791494352!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+
+                    <h6 class="site-footer-title mb-3">Social</h6>
+
+                    <ul class="social-icon">
+                        <li class="social-icon-item">
+                            <a href="https://www.instagram.com/nusambategallalang/" class="social-icon-link bi-instagram"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="https://www.facebook.com/bprnusambategallalang1990/" class="social-icon-link bi-facebook"></a>
+                        </li>
+
+                        <li class="social-icon-item">
+                            <a href="https://api.whatsapp.com/send/?phone=6281239726026&text=Tertarik+dengan+produk+PT.+BPR+Nusamaba+Tegallalang%3F&type=phone_number&app_absent=0" class="social-icon-link bi-whatsapp"></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
